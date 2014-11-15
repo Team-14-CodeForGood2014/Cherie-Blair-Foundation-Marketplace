0841ed445d49a4e1b59a418db8da67b56d0192bf
Andrew McCluskey <andrew.m.mccluskey@googlemail.com>
2014-11-15 03:09:37 +0000

    Preparing to write searchEngine script
    searchEngine script is ready to be written

--- Django/cbfm/searchEngine/views.py
+++ Django/cbfm/searchEngine/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from urlparse import urlparse, parse_qs
+
+import scripts
 
 # Create your views here.
 from django.http import HttpResponse
 
 def results(request):
-    return HttpResponse("This is the results potato")
+
+    #Find Query string
+
+#    requestParse = urlparse.parse_qs(request)
+    quantity = request.GET['quantity']
+    units = request.GET['units']
+    keywords = request.GET['keywords']
+    region = request.GET['region']
+    
+    return HttpResponse(scripts.main(quantity,units,keywords,region)))
